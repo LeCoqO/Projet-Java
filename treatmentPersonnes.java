bc4dfd8c9df33577af6fe4b6eae0dd95b6ad94eb
Kyuu71 <quentin.lustiere.auditeur@lecnam.net>
2022-05-16 14:30:24 +0200

    Create treatmentPersonnes.java

---
+++ treatmentPersonnes.java
@@ -0,0 +1,10 @@
+class treatmentPersonne{
+    
+    static String[] getUserByCategorie(String categorie){
+        //Appel de la DAO avec les bons param.
+        //Retour et traitement de ces données
+        String[] retour = "";
+        return retour; 
+        
+    } 
+}
