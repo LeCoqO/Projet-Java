b8ef87443308e785055a6b57ec989450c4fd1480
Spok3r <clement.fouchard.auditeur@lecnam.net>
2022-06-09 09:29:46 +0200

    Update Employe_Campagne.java

--- Sources/Entity/Employe_Campagne.java
+++ Sources/Entity/Employe_Campagne.java
@@ -6,6 +6,34 @@ public class Employe_Campagne {
     private int idEmploye;
     private boolean autorisation;
 
-   
+    public Employe_Campagne(int idCampagne, int idEmploye, boolean autorisation) {
+        this.idCampagne = idCampagne;
+        this.idEmploye = idEmploye;
+        this.autorisation = autorisation;
+    }
+
+    public int getIdCampagne() {
+        return idCampagne;
+    }
+
+    public void setIdCampagne(int idCampagne) {
+        this.idCampagne = idCampagne;
+    }
+
+    public int getIdEmploye() {
+        return idEmploye;
+    }
+
+    public void setIdEmploye(int idEmploye) {
+        this.idEmploye = idEmploye;
+    }
+
+    public boolean isAutorisation() {
+        return autorisation;
+    }
+
+    public void setAutorisation(boolean autorisation) {
+        this.autorisation = autorisation;
+    }
 
 }
