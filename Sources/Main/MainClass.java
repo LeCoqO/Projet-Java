bdc6e47d864b6c7faa5c3e3757fe08b2a57a0507
Spok3r <clement.fouchard.auditeur@lecnam.net>
2022-06-09 14:08:25 +0200

    Up tentative de connexion à la base

--- Sources/Main/MainClass.java
+++ Sources/Main/MainClass.java
@@ -3,10 +3,13 @@ package Main;
 import Connection.ConnectionBDD;
 import Connection.Connector;
 import Connection.ConnectorMySQL;
+import DAO.*;
+import Entity.Batiment;
+
 import java.sql.*;
 
 public class MainClass {
-    
+
     static boolean checkConnection(Connector connector) {
 
         if (ConnectionBDD.isInstanceOf(connector)) {
@@ -17,12 +20,19 @@ public class MainClass {
             return false;
         }
     }
-    
+
     public static void main(String[] args) {
 
-        Connection connection = ConnectionBDD.getInstance(new ConnectorMySQL());
-        System.out.println("Connexion réussi !");
-        checkConnection(new ConnectorMySQL());
+        // Connection connection = ConnectionBDD.getInstance(new ConnectorMySQL());
+        // System.out.println("Connexion réussi !");
+        // checkConnection(new ConnectorMySQL());
+
+        Connector connectorTest = new ConnectorMySQL();
+        Connection connectTest = ConnectionBDD.getInstance(connectorTest);
+        DAOBatiment daoBatiment = new DAOBatiment(connectTest);
+        Batiment batimentTest = daoBatiment.selectById(1);
+
+        System.out.println(batimentTest);
     }
 
 }
